c70c601cd3821696ddbfea85c7067ae7990f9e8d
aDavtyan13 <alla.davtyan99@gmail.com>
2019-10-30 13:22:57 +0400

    first page

---
+++ src/app/first-field/first-field.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'first-field',
+  templateUrl: './first-field.component.html',
+  styleUrls: ['./first-field.component.css']
+})
+export class FirstFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
