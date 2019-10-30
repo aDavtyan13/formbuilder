3bb2cbe10acbacdd410d88f039bab1b9e631c157
aDavtyan13 <alla.davtyan99@gmail.com>
2019-10-30 18:56:12 +0400

    adding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { FirstFieldComponent } from './first-field/first-field.component';
 
 import { FormsModule } from '@angular/forms'
+import { ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { FormsModule } from '@angular/forms'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/first-field/first-field.component.ts
+++ src/app/first-field/first-field.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {  FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'first-field',
@@ -6,10 +9,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first-field.component.css']
 })
 export class FirstFieldComponent implements OnInit {
+  myform: FormGroup;
+  username: FormControl;
+  lastName: FormControl;
+  email: FormControl;
+  password: FormControl;
+  language: FormControl;
 
-  constructor() { }
 
   ngOnInit() {
+    this.createFormControls();
+    this.createForm();
+  }
+
+  createFormControls() {
+    this.username = new FormControl('', Validators.required);
+    this.lastName = new FormControl('', Validators.required);
+    this.email = new FormControl('', [
+      Validators.required
+      
+    ]);
+    this.password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(8)
+    ]);
+  }
+
+  createForm() {
+    this.myform = new FormGroup({
+     
+        username: this.username,
+        lastName: this.lastName,
+     
+
+      email: this.email,
+      password: this.password,
+    });
+  }
+
+  onSubmit(){
+    if(this.myform.valid) {
+      console.log("Form Submitted!");
+      this.myform.reset();
+    }
   }
 
 }
